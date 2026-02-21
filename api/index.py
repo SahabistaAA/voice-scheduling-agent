8abe806ba02626045db2c0d3271057adbb8f7595
sahabista <sahabista.arkitanego@jkt1.ebdesk.com>
2026-02-21 23:09:57 +0700

    :bug: fix bug on defining entrypoint

--- api/index.py
+++ api/index.py
@@ -1,1 +1,11 @@
 from source.app import app
+from fastapi import FastAPI
+from mangum import Mangum
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"message": "Voice Scheduling Agent is running 🚀"}
+
+handler = Mangum(app)
