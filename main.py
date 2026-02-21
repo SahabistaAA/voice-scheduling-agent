a20fc5162fc98e77fcc557aaa742c292221ecf71
sahabista <sahabista.arkitanego@jkt1.ebdesk.com>
2026-02-21 22:59:42 +0700

    :bug: fix: add FastAPI() string to main.py to satisfy Nixpacks parser

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
 from source.app import app
 
-# This file exists solely to provide an entrypoint for Vercel's Python runtime.
+# This file exists solely to provide an entrypoint for Vercel and Railway's Python runtime.
+# Specifically, Railway's Nixpacks builder looks for the exact string: FastAPI(
+# app = FastAPI()
